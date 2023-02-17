6dfc4d5e322fd3040b3926713aeff1cc658fb5fe
Przemysław Włodkowski <przemyslaw@wlodkowski.pl>
2023-02-17 21:56:35 +0100

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -1,7 +1,1 @@
-#!/bin/sh
-cd ~/Workspace/konsultacje-pppl/
-sonar-scanner \
-  -Dsonar.projectKey=konsultacje-opl \
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=sqp_9c52e5e35efbc3b2e49eb9f21e89dbc577db3455
+
