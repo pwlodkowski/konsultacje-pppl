2411c7ba85b9e310e9360a28b258e42b8b29ef03
Przemysław Włodkowski <przemyslaw@wlodkowski.pl>
2023-02-17 20:13:54 +0100

    sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+  -Dsonar.projectKey=konsultacje-opl \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=sqp_9c52e5e35efbc3b2e49eb9f21e89dbc577db3455
